cd81b1acd0f4134ff00e67279dc55b20c9e2bb0f
site.synchro@gmail.com <site.synchro@gmail.com@97426990-d8d6-0629-23ab-ce75750e07d7>
2012-12-08 16:28:29 +0000

    a simple database connection

---
+++ branches/dbConnection.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author holyknight
+ */
+public class dbConnection {
+    public static Connection con = null;
+    public static Statement st = null;
+    public static ResultSet rs = null;
+    public static String url = "jdbc:mysql://localhost:3306/parkiran";
+    public static String user = "root";
+    public static String password = "rahasia";
+}
